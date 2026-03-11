68ba003955e77641bb55464f4df08d4880c9d70d
yohannes <yohannesg218@gmail.com>
2026-03-11 01:38:06 -0700

    2

--- class.ts
+++ class.ts
@@ -1,6 +1,6 @@
 import { log } from "node:console";
 import * as readline from 'readline/promises'
-import { stdin as input,stdout as output } from "node:process";
+import { stdin as input, stdout as output } from 'node:process';
 
 
 function messageApp(message: string) {
@@ -18,33 +18,21 @@ function multi(a: number, b: number): number {
 }
 console.log(multi(3, 5));
 
-let input = prompt("enter the day from(1-7):");
-let day: number = Number(input);
-
+const rl = readline.createInterface(input,output)
+async function main() {
+    const request = await rl.question("Enter the day from (1-7):");
+    let day =Number(request)
 switch (day) {
-  case 1:
-    console.log("monday");
+  case 1:console.log("monday");break;
+  case 2:console.log("thusday");break;
+  case 3:console.log("wonsday");break;
+  case 4:console.log("thursday");break;
+  case 5:console.log("frieday");break;
+  case 6:console.log("saturday");break;
+  case 7:console.log("sunday");
     break;
-  case 2:
-    console.log("thusday");
-    break;
-  case 3:
-    console.log("wonsday");
-    break;
-  case 4:
-    console.log("thursday");
-    break;
-  case 5:
-    console.log("frieday");
-    break;
-  case 6:
-    console.log("saturday");
-    break;
-  case 7:
-    console.log("sunday");
-    break;
-  default:
-    console.log("invalid input");
-    
-    break;
+  
 }
+rl.close();
+}
+main();
